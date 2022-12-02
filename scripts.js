65c578b40b8c40ebf66ddd879c2d5785edd9ca3a
Che Hongle <chehongle123@163.com>
2022-12-02 17:36:03 -0600

    final version

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function handleProject() {
+    window.location.href="project1.html";
+}
+
+function handleBio() {
+    window.location.href="bio.html";
+}
+
+function handleBack() {
+    window.location.href="index.html";
+}
